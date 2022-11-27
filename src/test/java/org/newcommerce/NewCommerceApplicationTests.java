7c8f7153f0530e67004801d58663c9d1815790a5
PandaHun <gnsgnsek@gmail.com>
2022-11-27 09:56:41 +0900

    project init

---
+++ src/test/java/org/newcommerce/NewCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.newcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewCommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
